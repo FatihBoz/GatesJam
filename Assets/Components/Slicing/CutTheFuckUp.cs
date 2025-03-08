ca022a02a550f72dab0cf9a00a5ca8eb878fcbe2
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2025-03-08 14:08:21 +0300

    FIXER and prefaber

--- Assets/Components/Slicing/CutTheFuckUp.cs
+++ Assets/Components/Slicing/CutTheFuckUp.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 
 public class CutTheFuckUp : MonoBehaviour
 {
-    public GameObject spritePrefab; // Kesilen parçalar için prefab
-
-
+    public bool sliceEnabled = false;
+    public GameObject spritePrefab;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Sol tık ile kesme
+        if (Input.GetMouseButtonDown(0) && sliceEnabled)
         {
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
