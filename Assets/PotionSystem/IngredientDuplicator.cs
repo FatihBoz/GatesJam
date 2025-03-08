58ef9c3e2538de030c95950c16632a699bc07eb9
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2025-03-08 17:26:09 +0300

    Inventory System Started

---
+++ Assets/PotionSystem/IngredientDuplicator.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class IngredientDuplicator : MonoBehaviour
+{
+    public string ingrName;
+    //[SerializeField] private GameObject InventoryItem;
+    [SerializeField] private Camera mainCam;
+    GameObject currentIngredient;
+    private bool isDragging;
+
+    private void OnMouseDown()
+    {
+        print("on mouse down");
+        getItem();
+        isDragging = true;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            currentIngredient.transform.position = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            currentIngredient.transform.position = new Vector3(currentIngredient.transform.position.x, currentIngredient.transform.position.y, 0);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        dropItem();
+        Debug.Log("Item deleted from Inventory");
+
+    }
+
+    public void getItem()
+    {
+        InventoryItem inventoryItem;
+        inventoryItem = InventorySystem.instance.SearchItem(ingrName);
+        if (inventoryItem != null)
+        {
+            if (inventoryItem.quantity > 0)
+            {
+                currentIngredient = Instantiate(inventoryItem.ingredient.ingrPrefab, transform.position, Quaternion.identity);
+                InventorySystem.instance.DelItem(ingrName, 1);
+
+                Debug.Log("Item Spawned and reduced quantity from Inventory. ");
+            }
+        }
+
+
+
+    }
+
+    public void dropItem()
+    {
+        InventorySystem.instance.DelItem(ingrName.ToString(), 1);
+    }
+}
--- Assets/PotionSystem/Scripts/InventorySystem.cs
+++ Assets/PotionSystem/Scripts/InventorySystem.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 using static UnityEditor.Progress;
 
@@ -6,8 +8,10 @@ public class InventorySystem : MonoBehaviour
 {
     public static InventorySystem instance;
 
-    public List<InventoryItem> inventoryItems = new List<InventoryItem>();
+    //public GameObject currentItem;
 
+    List<InventoryItem> Inventory = new List<InventoryItem>();
+    public List<Ingredients> ingredients;
     private void Awake()
     {
         // Check if an instance already exists
@@ -22,4 +26,74 @@ public class InventorySystem : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void Start()
+    {
+        DefineItemsToInventory();
+    }
+
+    public void DefineItemsToInventory()
+    {
+        foreach(Ingredients ingr in ingredients)
+        {
+            InventoryItem item = new InventoryItem();
+
+            item.ingredient = ingr;  // Ingredients sýnýfý tanýmlý olmalý
+            item.quantity = 10;
+
+            Inventory.Add(item);
+
+            //Inventory.Add(new InventoryItem { ingredient = ingr, quantity = 99 });
+
+        }
+    }
+    
+    public void AddItem(string ItemName, int quantity)
+    {
+        foreach (var data in Inventory)
+        {
+            if (data.ingredient.ingrName == ItemName)
+            {
+                data.quantity += quantity;
+                return;
+            }
+        }
+    }
+
+    public void DelItem(string ItemName, int quantity)
+    {
+        foreach (var data in Inventory)
+        {
+            if (data.ingredient.ingrName == ItemName)
+            {
+                data.quantity -= quantity;
+                return;
+            }
+        }
+    }
+
+    public void RandomlyGiveItems()
+    {
+        int randomNum = 0;
+        for(int i=0; i<Inventory.Count; i++)
+        {
+
+            randomNum = Random.Range(0, 3);
+
+            Inventory[i].quantity = randomNum;
+        }
+    }
+
+    public InventoryItem SearchItem(string ItemName)
+    {
+        foreach (var data in Inventory)
+        {
+            if (data.ingredient.ingrName == ItemName)
+            {
+                return data;
+            }
+        }
+        return null;
+    }
+
 }
